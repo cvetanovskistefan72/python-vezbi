cd7c7a5eb7bbbb2bbe954845e60a6e149a57e1a6
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-15 21:31:26 +0100

    parni resena

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ lista = [1,2,3,4,5,6]
 #TODO sortiranje na lista
 lista.reverse()
 #TODO izvadi parni neparni
+
+parni = []
+for item in lista:
+    if item % 2 == 0:
+        parni.append(item)
+
+
+print(parni)
