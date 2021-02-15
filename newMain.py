c7ab28967ada3d4042af8e04de3faeb38b7dfd58
Grozdanovsky <viktor.grozdanovski@outlook.com>
2021-02-15 22:02:21 +0100

    novi lugje

--- newMain.py
+++ newMain.py
@@ -2,5 +2,13 @@ from classes.Person import *
 
 #MAIN
 
-chovek = Person("Stefan","Cvetanovski")
+chovek1 = Person("Stefan","Cvetanovski")
+
+chovek2 = Person("Viktor","Grozdanovski")
+
+chovek3 = Person("Bojan","Stojanovski")
+
+chovek4 = Person("Filip","Gicehvski")
+
+chovek5 = Person("Petra", "Ivanovska")
 
